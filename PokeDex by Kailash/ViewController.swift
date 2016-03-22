cff122b1a0b9478ed9f1ddbf955c1a036e0386f0
Kailash <kailashrkk@gmail.com>
2016-03-22 11:54:57 -0400

    first commit

---
+++ PokeDex by Kailash/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokeDex by Kailash
+//
+//  Created by Kailash Ramaswamy Krishna Kumar on 3/22/16.
+//  Copyright © 2016 Kailash Ramaswamy Krishna Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
